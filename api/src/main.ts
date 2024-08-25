b3ff4f51af6036b11d1681be7cb243ee9eb27cf2
jeongjihun <wlgns2234@naver.com>
2024-08-26 06:22:53 +0900

    feat: 모듈 import
    - npm run build(트랜스파일)
    - npm run start(시작)

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,2 +1,9 @@
-console.log("hello typescript!")
+import Product from "./types/Product";
 
+console.log("hello typescript!");
+
+const product: Product = {
+  id: 1,
+  name: "name",
+}
+console.log(product);
